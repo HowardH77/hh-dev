619ebe403e1130d98590f1d88efe6bb23627a3ca
HowardH77 <hmail.98@gmail.com>
2018-09-26 05:00:01 -0400

    Create printargs.c

---
+++ bin/Src/printargs.c
@@ -0,0 +1,57 @@
+/* Name:        %I%
+ * Synopsis:    printargs -c -i -q commandline
+ * Description: printargs displays cmdline arguments.  This is useful for debugging 
+ *              shell scripts that escape the space, backslash, quote chars ('"`)
+ *              and other wildcard characters (eg: "[*?]").
+ *                Options: -c  Echo the command line
+ *                         -i  Do not display argument indices
+ *                         -q  Do not display single quotes surrounding each argument
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+main(int argc, char *argv[])
+{
+	int opt;
+	extern int optind;
+	int echo_cmdline = 0; 
+	int display_indices = 1; 
+	int indx;
+	char *quotes = "'";
+	char *spaces = "";
+
+	while ((opt = getopt(argc, argv, "ciq")) != EOF) {
+		switch(opt) {
+		case 'c': 
+			echo_cmdline = 1;
+			break;
+		case 'i':
+			display_indices = 0;
+			break;
+		case 'q':
+			quotes = "";
+			break;
+		default:
+			fprintf(stderr, "%s: Invalid option '%c'\n", opt);
+			fprintf(stderr, "Syntax: %s [-ciq] command command_options command_args ...");
+			exit(1);
+		}
+	}
+
+	--argc;
+	if (echo_cmdline == 1) {
+		printf("CmdLine: ");
+		for (indx = optind; indx <= argc; ++indx)
+			printf("%s%c", argv[indx], indx < argc ? ' ' : '\n');
+	}
+
+
+	for (indx = optind; indx <= argc; ++indx) {
+		if (display_indices == 1)
+			printf("%d : ", indx - optind + 1);
+		printf("%s%s%s\n", quotes, argv[indx], quotes);
+	}
+
+	exit(0);
+}
